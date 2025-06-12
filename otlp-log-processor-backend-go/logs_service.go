31022356b9f130d9e429f733db537a0dc04d935e
Jose Pereira <jose.pereira@olx.com>
2025-06-12 10:41:32 +0100

    Add processor

--- otlp-log-processor-backend-go/logs_service.go
+++ otlp-log-processor-backend-go/logs_service.go
@@ -5,24 +5,35 @@ import (
 	"log/slog"
 
 	collogspb "go.opentelemetry.io/proto/otlp/collector/logs/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type dash0LogsServiceServer struct {
-	addr string
-
 	collogspb.UnimplementedLogsServiceServer
+
+	logsProcessor *logsProcessor
 }
 
-func newServer(addr string) collogspb.LogsServiceServer {
-	s := &dash0LogsServiceServer{addr: addr}
-	return s
+func newServer(
+	logsProcessor *logsProcessor,
+) collogspb.LogsServiceServer {
+	return &dash0LogsServiceServer{
+		logsProcessor: logsProcessor,
+	}
 }
 
-func (l *dash0LogsServiceServer) Export(ctx context.Context, request *collogspb.ExportLogsServiceRequest) (*collogspb.ExportLogsServiceResponse, error) {
+func (l *dash0LogsServiceServer) Export(
+	ctx context.Context,
+	request *collogspb.ExportLogsServiceRequest,
+) (*collogspb.ExportLogsServiceResponse, error) {
 	slog.DebugContext(ctx, "Received ExportLogsServiceRequest")
+
 	logsReceivedCounter.Add(ctx, 1)
 
-	// Do something with the logs
+	if err := l.logsProcessor.processLogs(request); err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
 
 	return &collogspb.ExportLogsServiceResponse{}, nil
 }
